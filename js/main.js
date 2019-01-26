68fb2eb091cf8406518209ee4f87a8e4ce85ea3e
neohotsauce <46462510+neohotsauce@users.noreply.github.com>
2019-01-27 01:44:41 +0500

    live 27-01-19

--- js/main.js
+++ js/main.js
@@ -19,7 +19,32 @@ var typed2 = new Typed("#typed2", {
 });
 
 $(document).ready(function() {
-  $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function() {
+  $(".nav-icon4").click(function() {
     $(this).toggleClass("open");
   });
 });
+
+function calculatePrice() {
+  var weight = document.getElementById("total-weight").value;
+  var price = document.getElementById("total-price").value;
+  var weight_unit = document.getElementsByName("weight-unit");
+  var rate_value;
+  for (var i = 0; i < weight_unit.length; i++) {
+    if (weight_unit[i].checked) {
+      rate_value = weight_unit[i].value;
+    }
+  }
+  var totalprice = (+weight * +rate_value + +price * 1.12) * 1.06;
+  var finalprice = totalprice.toFixed(2);
+  document.getElementById("final-price").innerHTML = "$" + finalprice;
+}
+
+function showCalculator() {
+  var element = document.getElementById("calculator-popup");
+  element.classList.add("show-calculator");
+}
+
+function closeCalculator() {
+  var element = document.getElementById("calculator-popup");
+  element.classList.remove("show-calculator");
+}
